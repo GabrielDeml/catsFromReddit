5b6e7a23c5ec0831e8200bf58388730ee44cd367
GabrielDeml <gabrielddeml@gmail.com>
2020-03-17 16:37:05 -0400

    cleaned up

--- aww.py
+++ aww.py
@@ -11,39 +11,44 @@ import time
 try:
     # Python2
     from urllib2 import urlopen
+    import Tkinter
+
+    tkinter = Tkinter
 except ImportError:
     # Python3
     from urllib.request import urlopen
-
-if sys.version_info[0] == 2:  # the tkinter library changed it's name from Python 2 to 3.
-    import Tkinter
-
-    tkinter = Tkinter  # I decided to use a library reference to avoid potential naming conflicts with people's programs.
-else:
     import tkinter
 
-root = tkinter.Tk()
-screen_width, screen_height = root.winfo_screenwidth(), root.winfo_screenheight()
+# Change me to your screen resolution
+screen_width = 1920
+screen_height = 1080
 
+# Change this to change how long to display the image
+timeToWait = 2
+
+# In it pygame
 pg.init()
+clock = pg.time.Clock()
+
+# Get user agent and stuff
 ua = UserAgent(
     fallback='Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11')
+
+# Reddit url to get images and how many to get
+# Change limit if you want more
 url = 'https://www.reddit.com/r/cats.json?limit=10000'
 response = requests.get(url, headers={'User-agent': ua.random})
 
+# Make sure we got a good response
 if not response.ok:
     print("Error", response.status_code)
     exit()
 data = response.json()['data']['children']
 
-clock = pg.time.Clock()
 
-
-def show_image(image_url):
-    image_str = urlopen(image_url).read()
+def show_image(inputImage):
     # create a file object (stream)
-    image_file = io.BytesIO(image_str)
-    print(image_file)
+    image_file = io.BytesIO(inputImage)
     # (r, g, b) color tuple, values 0 to 255
     black = (0, 0, 0)
     # create a 600x400 black screen
@@ -51,39 +56,33 @@ def show_image(image_url):
     screen.fill(black)
     # load the image from a file or stream
     image = pg.image.load(image_file)
-    # image = pg.transform.scale(image, findsize(image.get_size()))
-    print(image.get_size())
-    print(findsize(image.get_size()))
+    # Scale the image
     image = pg.transform.scale(image, findsize(image.get_size()))
+    # Move the image to the center of the screen
     rect = image.get_rect()
-    rect = rect.move(((1920/2) - (image.get_size()[0]/2), 0))
-
+    rect = rect.move(((screen_width / 2) - (image.get_size()[0] / 2), 0))
     screen.blit(image, rect)
+    # Display the image
     pg.display.update()
-    time.sleep(2)
+    # Wait for time
+    time.sleep(timeToWait)
+
 
 def findsize(size):
-    # if (size[1] >= size[0]):
-    delta = 1080/size[1]
-    width = int(round(size[0]*delta))
-    height = int(round(size[1]*delta))
-    # print("first")
+    delta = screen_height / size[1]
+    width = int(round(size[0] * delta))
+    height = int(round(size[1] * delta))
     return width, height
-    # # else:
-    #     delta = 1920 / size[0]
-    #     width = int(round(size[0] * delta))
-    #     height = int(round(size[1] * delta))
-    #     # print("first")
-    #     return width, height
 
 
+# Forever get the images
 while True:
     for i in range(len(data)):
         current_post = data[i]['data']
         image_url = current_post['url']
         image = requests.get(image_url, allow_redirects=False)
-        if (image.status_code == 200):
+        if image.status_code == 200:
             try:
-                show_image(image_url)
+                show_image(image.content)
             except:
                 pass
